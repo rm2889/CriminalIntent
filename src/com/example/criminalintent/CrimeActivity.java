46e2c1beed5c4e7401e73eba984c7b586d787be3
Raghav Malik <raghav1987@gmail.com>
2014-08-24 13:59:19 +0530

    Chapter 7

--- src/com/example/criminalintent/CrimeActivity.java
+++ src/com/example/criminalintent/CrimeActivity.java
@@ -1,36 +1,25 @@
 package com.example.criminalintent;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.app.Fragment;
+import android.app.FragmentManager;
 
 
-public class CrimeActivity extends ActionBarActivity {
+public class CrimeActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_crime);
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.crime, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+		FragmentManager fm = getFragmentManager();
+		Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+		
+		if (fragment == null) {
+			fragment = new CrimeFragment();
+			fm.beginTransaction()
+			.add(R.id.fragmentContainer, fragment)
+			.commit(); }
+	}
 }
